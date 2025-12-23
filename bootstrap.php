bc2bc9654065d0b738c1cc59b8e4f3eaf3778bb9
weijian.ye <yeweijian299@163.com>
2025-12-23 17:40:07 +0800

    init

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,3 @@
 <?php
-/**
- * bootstrap.php
- * PHP version 7
- *
- * @package openai-web
- * @author  weijian.ye
- * @contact yeweijian@eyugame.com
- * @link    https://github.com/vzina
- */
-declare (strict_types=1);
 
-var_dump(443);
+\Vzina\Attributes\AttributeLoader::init();
