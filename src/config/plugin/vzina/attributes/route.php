9172295c150f46ad64f396dc6ebaf5945ee4fab3
weijian.ye <yeweijian299@163.com>
2025-12-29 15:46:43 +0800

    init

--- src/config/plugin/vzina/attributes/route.php
+++ src/config/plugin/vzina/attributes/route.php
@@ -1,13 +1,3 @@
 <?php
-/**
- * route.php
- * PHP version 7
- *
- * @package attributes
- * @author  weijian.ye
- * @contact 891718689@qq.com
- * @link    https://github.com/vzina
- */
-declare (strict_types=1);
 
 Vzina\Attributes\Attribute\Route\DispatcherFactory::init();
